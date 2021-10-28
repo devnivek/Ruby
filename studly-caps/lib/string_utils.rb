80e13bd1885848b019d66f1627fc85c012b34030
devnivek <kevin.santos@outlook.com>
2021-10-28 00:48:21 -0300

    adicionando desafios concluidos

--- studly-caps/lib/string_utils.rb
+++
@@ -1,7 +0,0 @@
-class StringUtils
-  def wavefy(string)
-    c = string.chars    
-    string = c.map.with_index{ |item,i| i.odd? ? item.upcase : item.downcase }
-    string.join 
-  end
-end
--- sum-numbers/lib/numbers.rb
+++
@@ -1,7 +0,0 @@
-class Numbers
-  def sum_text(numbers_text)
-    numeros = numbers_text.split("\n")
-    soma = numeros.map(&:to_i)
-    numbers_text = soma.sum
-  end
-end
